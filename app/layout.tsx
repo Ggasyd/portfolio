f9b1b0f40be8b2f8f904dff35305838d31843b59
Ggasyd <149815357+Ggasyd@users.noreply.github.com>
2024-04-06 10:27:42 +0200

    Updated layout.tsx
    Analytics insights for Vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,8 @@ import Footer from "@/components/footer";
 import ThemeSwitch from "@/components/theme-switch";
 import ThemeContextProvider from "@/context/theme-context";
 import { Toaster } from "react-hot-toast";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -32,6 +33,7 @@ export default function RootLayout({
           <ActiveSectionContextProvider>
             <Header />
             {children}
+            <Analytics />
             <SpeedInsights />
             <Footer />
 
